8e95216dd8b785b7fc2be55619bf6e36da60da1c
Drew <drewsyp@gmail.com>
2022-04-01 11:39:15 -0700

    Initial commitafter transfer from AVLTree

---
+++ core/AVLBag.hpp
@@ -0,0 +1,475 @@
+// AVLBag.hpp
+#ifndef AVLBAG_HPP
+#define AVLBAG_HPP
+
+#include <functional>
+#include <algorithm>
+#include <iostream>
+
+template <typename ElementType>
+class AVLBag
+{
+public:
+    // A VisitFunction is a function that takes a reference to a const
+    // ElementType and returns no value.
+    using VisitFunction = std::function<void(const ElementType&)>;
+
+public:
+    // Initializes an AVLBag to be empty.
+    explicit AVLBag();
+
+    // Cleans up the AVLBag so that it leaks no memory.
+    ~AVLBag() noexcept;
+
+    // Initializes a new AVLBag to be a copy of ana existing one.
+    AVLBag(const AVLBag& s);
+
+    // Initializes a new AVLBag whose contents are moved from an
+    // expiring one.
+    AVLBag(AVLBag&& s) noexcept;
+
+    // Assigns an existing AVLBag into another.
+    AVLBag& operator=(const AVLBag& s);
+
+    // Assigns an expiring AVLBag into another.
+    AVLBag& operator=(AVLBag&& s) noexcept;
+
+    // add() adds an element to the set.  If the element is already in the set,
+    // this function has no effect.  This function always runs in O(log n) time
+    // when there are n elements in the AVL tree.
+    void add(const ElementType& element);
+
+
+    // contains() returns true if the given element is already in the set,
+    // false otherwise.  This function always runs in O(log n) time when
+    // there are n elements in the AVL tree.
+    bool contains(const ElementType& element) const;
+
+
+    // size() returns the number of elements in the set.
+    unsigned int size() const noexcept;
+
+
+    // height() returns the height of the AVL tree.  Note that, by definition,
+    // the height of an empty tree is -1.
+    int height() const noexcept;
+
+
+    // amount() returns the amount of objects in that "bag". 
+    // Duplicate objecs will get added to their respective bag.
+    int amount(const ElementType& element) const;
+
+
+    // preorder() calls the given "visit" function for each of the elements
+    // in the set, in the order determined by a preorder traversal of the AVL
+    // tree.
+    void preorder(VisitFunction visit) const;
+
+
+    // inorder() calls the given "visit" function for each of the elements
+    // in the set, in the order determined by an inorder traversal of the AVL
+    // tree.
+    void inorder(VisitFunction visit) const;
+
+
+    // postorder() calls the given "visit" function for each of the elements
+    // in the set, in the order determined by a postorder traversal of the AVL
+    // tree.
+    void postorder(VisitFunction visit) const;
+
+
+private:
+    struct Node
+    {
+        ElementType data;
+        Node* left;
+        Node* right;
+        unsigned int amount;
+    };
+
+    Node* origin;
+    unsigned int nodes;
+    bool isBalanced;
+
+    void cleanupRecursive(const Node* node);
+    int heightOfTree(const Node* node) const noexcept;
+    Node* RotateRight(Node* node);
+    Node* RotateLeft(Node* node);
+    Node* addRecursive(Node* node, const ElementType& element);
+    bool containsRecursive(const Node* node, const ElementType& element) const;
+    int amountRecursive(const Node* node) const;
+    void preorderRecursive(VisitFunction visit, const Node* node) const;
+    void inorderRecursive(VisitFunction visit, const Node* node) const;
+    void postorderRecursive(VisitFunction visit, const Node* node) const;
+    void copyTree(const Node* node);
+};
+
+
+template <typename ElementType>
+AVLBag<ElementType>::AVLBag()
+    : origin{nullptr}, nodes{0}, isBalanced{false}
+{
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::cleanupRecursive(const Node* node)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    if (node->left != nullptr)
+    {
+        cleanupRecursive(node->left);
+    }
+    if (node->right != nullptr)
+    {
+        cleanupRecursive(node->right);
+    }
+    delete node;
+    return;
+}
+
+
+template <typename ElementType>
+AVLBag<ElementType>::~AVLBag() noexcept
+{
+    cleanupRecursive(origin);
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::copyTree(const Node* node)
+{
+    // base case
+    if (node == nullptr)
+    {
+        return;
+    }
+    // add itself
+    add(ElementType{node->data});
+    // add its left child
+    if (node->left != nullptr)
+    {
+        copyTree(node->left);
+    }
+    // add its right child
+    if (node->right != nullptr)
+    {
+        copyTree(node->right);
+    }
+}
+
+
+template <typename ElementType>
+AVLBag<ElementType>::AVLBag(const AVLBag& s)
+    : origin{nullptr}, nodes{0}, isBalanced{false}
+{
+    copyTree(s.origin);
+}
+
+
+template <typename ElementType>
+AVLBag<ElementType>::AVLBag(AVLBag&& s) noexcept
+    : origin{nullptr}, nodes{0}, isBalanced{false}
+{
+    std::swap(origin, s.origin);
+    std::swap(nodes, s.nodes);
+}
+
+
+template <typename ElementType>
+AVLBag<ElementType>& AVLBag<ElementType>::operator=(const AVLBag& s)
+{
+    if (this != &s)
+    {
+        // cleanup old stuff
+        cleanupRecursive(origin);
+        origin = nullptr;
+        nodes = 0;
+        isBalanced = false;
+        // copy it over
+        copyTree(s.origin);
+    }
+    return *this;
+}
+
+
+template <typename ElementType>
+AVLBag<ElementType>& AVLBag<ElementType>::operator=(AVLBag&& s) noexcept
+{
+    if (this != &s)
+    {
+        // cleanup old stuff
+        cleanupRecursive(origin);
+        origin = nullptr;
+        nodes = 0;
+        isBalanced = false;
+        // swap it
+        std::swap(nodes, s.nodes);
+        std::swap(origin, s.origin);
+    }
+    return *this;
+}
+
+
+template <typename ElementType>
+int AVLBag<ElementType>::heightOfTree(const Node* node) const noexcept
+{
+    if (node == nullptr)
+    {
+        return -1;
+    }
+    else if (node->left == nullptr && node->right == nullptr)
+    {
+        return 0;
+    }
+    return 1 + std::max(heightOfTree(node->left), heightOfTree(node->right));
+}
+
+
+template <typename ElementType>
+typename AVLBag<ElementType>::Node* AVLBag<ElementType>::RotateRight(Node* node)
+{
+    // a is b's (node) left child
+    Node* a = node->left;
+    // t2 is a's right child
+    Node* t2 = a->right;
+    // a's right child is now b
+    a->right = node;
+    // b's left child is now t2
+    node->left = t2;
+    // return new "origin"
+    return a;
+}
+
+
+template <typename ElementType>
+typename AVLBag<ElementType>::Node* AVLBag<ElementType>::RotateLeft(Node* node)
+{
+    // b is a's (node) right child
+    Node* b = node->right;
+    // t2 is b's left child
+    Node* t2 = b->left;
+    // b's left child is now a
+    b->left = node;
+    // a's right child is now t2
+    node->right = t2;
+    // return new "origin"
+    return b;
+}
+
+
+template <typename ElementType>
+typename AVLBag<ElementType>::Node* AVLBag<ElementType>::addRecursive(Node* node, const ElementType& element)
+{
+    // base case
+    if (node == nullptr)
+    {
+        nodes += 1;
+        return new Node{element, nullptr, nullptr};
+    }
+    if (element > node->data)
+    {
+        node->right = addRecursive(node->right, element);
+    }
+    else if (element < node->data)
+    {
+        node->left = addRecursive(node->left, element);
+    }
+    else
+    {
+        // if we run into a duplicate
+        node.amount += 1;
+        return node;
+    }
+    // This is here to save time. Because we will only ever have to rebalance once,
+    // we can ensure that once we rebalance, we don't spend time checking if it is unbalanced
+    // and instead recursively unwind.
+    if (isBalanced == false)
+    {
+        // if you image the tree like a number line, where left is negative
+        // and right is positive, this represents the bais towards one side.
+        // if its skewed left it will be positive, if its skewed right it will
+        // be negative.
+        int skew = heightOfTree(node->left) - heightOfTree(node->right);
+        // Skewed Right
+        if (skew <= -2)
+        {
+            // Skewed Right
+            if (element > node->right->data)
+            {
+                // rotate it left to fix
+                isBalanced = true;
+                return RotateLeft(node);
+            }
+            // Skewed Left
+            if (element < node->right->data)
+            {
+                // rotate it right then left to fix
+                isBalanced = true;
+                node->right = RotateRight(node->right);
+                return RotateLeft(node);
+            }
+        }
+        // Skewed Left
+        if (skew >= 2)
+        {
+            // Skewed Right
+            if (element > node->left->data)
+            {
+                // rotate it left then right to fix it
+                isBalanced = true;
+                node->left = RotateLeft(node->left);
+                return RotateRight(node);
+            }
+            // Skewed Left
+            if (element < node->left->data)
+            {
+                // rotate it right to fix
+                isBalanced = true;
+                return RotateRight(node);
+            }
+        }
+    }
+    // if not we aren't balancing we just
+    // return the node
+    return node;
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::add(const ElementType& element)
+{
+    isBalanced = false;
+    origin = addRecursive(origin, element);
+}
+
+
+template <typename ElementType>
+bool AVLBag<ElementType>::containsRecursive(const Node* node, const ElementType& element) const
+{
+    if (node == nullptr)
+    {
+        return false;
+    }
+    if (element < node->data)
+    {
+        return containsRecursive(node->left, element);
+    }
+    if (element > node->data)
+    {
+        return containsRecursive(node->right, element);
+    }
+    return element == node->data;
+    
+}
+
+
+template <typename ElementType>
+bool AVLBag<ElementType>::contains(const ElementType& element) const
+{
+    return containsRecursive(origin, element);
+}
+
+
+template <typename ElementType>
+unsigned int AVLBag<ElementType>::size() const noexcept
+{
+    return nodes;
+}
+
+
+template <typename ElementType>
+int AVLBag<ElementType>::height() const noexcept
+{
+    return heightOfTree(origin);
+}
+
+
+template <typename ElementType>
+int AVLBag<ElementType>::amountRecursive(const Node* node) const
+{
+
+}
+
+
+template <typename ElementType>
+int AVLBag<ElementType>::amount(const ElementType& element) const
+{
+
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::preorderRecursive(VisitFunction visit, const Node* node) const
+{
+    visit(node->data);
+    if (node->left != nullptr)
+    {
+        preorderRecursive(visit, node->left);
+    }
+    if (node->right != nullptr)
+    {
+        preorderRecursive(visit, node->right);
+    }
+    return;
+}
+
+template <typename ElementType>
+void AVLBag<ElementType>::preorder(VisitFunction visit) const
+{
+    preorderRecursive(visit, origin);
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::inorderRecursive(VisitFunction visit, const Node* node) const
+{
+    if (node->left != nullptr)
+    {
+        inorderRecursive(visit, node->left);
+    }
+    visit(node->data);
+    if (node->right != nullptr)
+    {
+        inorderRecursive(visit, node->right);
+    }
+    return;
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::inorder(VisitFunction visit) const
+{
+    inorderRecursive(visit, origin);
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::postorderRecursive(VisitFunction visit, const Node* node) const
+{
+    if (node->left != nullptr)
+    {
+        postorderRecursive(visit, node->left);
+    }
+    if (node->right != nullptr)
+    {
+        postorderRecursive(visit, node->right);
+    }
+    visit(node->data);
+    return;
+}
+
+
+template <typename ElementType>
+void AVLBag<ElementType>::postorder(VisitFunction visit) const
+{
+    postorderRecursive(visit, origin);
+}
+
+
+
+#endif
+
---
+++ gtest/AVLBagTests.cpp
@@ -0,0 +1,317 @@
+// AVLBagTests.cpp
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include "AVLBag.hpp"
+
+
+TEST(AVLBagTests, canCreateAndDestroy)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+}
+
+
+TEST(AVLBagTests, canCreateAndCleanup)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+    s1.add(10);
+    s2.add("stuff");
+}
+
+
+TEST(AVLBagTests, heightOfEmptyIsNegativeOne)
+{
+    AVLBag<int> s;
+    ASSERT_EQ(-1, s.height());
+}
+
+
+TEST(AVLBagTests, canCopyConstructToCompatibleType)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+
+    AVLBag<int> s1Copy{s1};
+    AVLBag<std::string> s2Copy{s2};
+}
+
+
+TEST(AVLBagTests, canCopyConstruct)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+    AVLBag<int> s1Copy{s1};
+    for (int x = 0; x < 100; ++x)
+    {
+        ASSERT_TRUE(s1Copy.contains(x));
+    }
+    ASSERT_EQ(s1.height(), s1Copy.height());
+    ASSERT_EQ(s1.size(), s1Copy.size());
+    
+    AVLBag<std::string> s2;
+    s2.add("a");
+    s2.add("b");
+    s2.add("c");
+    s2.add("d");
+    AVLBag<std::string> s2Copy{s2};
+    ASSERT_TRUE(s2Copy.contains("a"));
+    ASSERT_TRUE(s2Copy.contains("b"));
+    ASSERT_TRUE(s2Copy.contains("c"));
+    ASSERT_TRUE(s2Copy.contains("d"));
+    ASSERT_EQ(s2.height(), s2Copy.height());
+    ASSERT_EQ(s2.size(), s2Copy.size());
+}
+
+
+TEST(AVLBagTests, canMoveConstructToCompatibleType)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+
+    AVLBag<int> s1Copy{std::move(s1)};
+    AVLBag<std::string> s2Copy{std::move(s2)};
+}
+
+
+TEST(AVLBagTests, canMoveConstruct)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+    AVLBag<int> s1Copy{std::move(s1)};
+    for (int x = 0; x < 100; ++x)
+    {
+        ASSERT_TRUE(s1Copy.contains(x));
+    }
+    ASSERT_EQ(100, s1Copy.size());
+    
+    AVLBag<std::string> s2;
+    s2.add("a");
+    s2.add("b");
+    s2.add("c");
+    s2.add("d");
+    AVLBag<std::string> s2Copy{std::move(s2)};
+    ASSERT_TRUE(s2Copy.contains("a"));
+    ASSERT_TRUE(s2Copy.contains("b"));
+    ASSERT_TRUE(s2Copy.contains("c"));
+    ASSERT_TRUE(s2Copy.contains("d"));
+    ASSERT_EQ(4, s2Copy.size());
+}
+
+
+TEST(AVLBagTests, canAssignToCompatibleType)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+
+    AVLBag<int> s3;
+    AVLBag<std::string> s4;
+
+    s1 = s3;
+    s2 = s4;
+}
+
+
+TEST(AVLBagTests, canAssignConstruct)
+{
+    AVLBag<int> s1;
+    AVLBag<int> s1Copy;
+    for (int x = 0; x < 50; ++x)
+    {
+        s1.add(x);
+    }
+    for (int x = -100; x < 0; ++x)
+    {
+        s1Copy.add(x);
+    }
+    s1Copy = s1;
+    s1.add(50);
+    s1.add(51);
+    for (int x = 0; x < 50; ++x)
+    {
+        ASSERT_TRUE(s1Copy.contains(x));
+    }
+    for (int x = -100; x < 0; ++x)
+    {
+        ASSERT_FALSE(s1Copy.contains(x));
+    }
+    ASSERT_EQ(50, s1Copy.size());
+    
+    AVLBag<std::string> s2;
+    AVLBag<std::string> s2Copy;
+    s2.add("a");
+    s2.add("b");
+    s2Copy = s2;
+    s2.add("c");
+    s2.add("d");
+    ASSERT_TRUE(s2Copy.contains("a"));
+    ASSERT_TRUE(s2Copy.contains("b"));
+    ASSERT_FALSE(s2Copy.contains("c"));
+    ASSERT_FALSE(s2Copy.contains("d"));
+    ASSERT_EQ(2, s2Copy.size());
+}
+
+
+
+TEST(AVLBagTests, canMoveAssignToCompatibleType)
+{
+    AVLBag<int> s1;
+    AVLBag<std::string> s2;
+
+    AVLBag<int> s3;
+    AVLBag<std::string> s4;
+
+    s1 = std::move(s3);
+    s2 = std::move(s4);
+}
+
+
+TEST(AVLBagTests, containsElementsAfterAdding)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+    for (int x = 0; x < 100; ++x)
+    {
+        ASSERT_TRUE(s1.contains(x));
+    }
+    ASSERT_FALSE(s1.contains(101));
+    ASSERT_FALSE(s1.contains(-1));
+}
+
+
+TEST(AVLBagTests, doesNotContainElementsNotAdded)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 50; ++x)
+    {
+        s1.add(x);
+    }
+    for (int x = 51; x < 100; ++x)
+    {
+        ASSERT_FALSE(s1.contains(x));
+    }
+}
+
+
+TEST(AVLBagTests, canAddDuplicates)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 10; ++x)
+    {
+        s1.add(x);
+    }
+    for (int x = 0; x < 10; ++x)
+    {
+        s1.add(x);
+    }
+    ASSERT_EQ(3, s1.height());
+    ASSERT_EQ(10, s1.size());
+    AVLBag<int> s2;
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+    ASSERT_EQ(100, s1.size());
+}
+
+
+TEST(AVLBagTests, sizeIsNumberOfElementsAdded)
+{
+    AVLBag<int> s1;
+    s1.add(11);
+    s1.add(1);
+    s1.add(5);
+
+    EXPECT_EQ(3, s1.size());
+}
+
+
+TEST(AVLBagTests, heightDependsOnBalancing)
+{
+    AVLBag<int> balanced{};
+    balanced.add(1);
+    balanced.add(2);
+    balanced.add(3);
+    balanced.add(4);
+
+    EXPECT_EQ(2, balanced.height());
+}
+
+
+TEST(AVLBagTests, preOrderTraversal)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 10; ++x)
+    {
+        s1.add(x);
+    }
+    std::vector<int> preElements;
+    s1.preorder([&](const int& element) { preElements.push_back(element); });
+    std::vector<int> expectedPreElements{3, 1, 0, 2, 7, 5, 4, 6, 8, 9};
+
+    for (unsigned int i = 0; i < 9; ++i)
+    {
+        EXPECT_EQ(preElements[i], expectedPreElements[i]);
+    }
+}
+
+
+TEST(AVLBagTests, inOrderTraversal)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 10; ++x)
+    {
+        s1.add(x);
+    }
+    std::vector<int> inElements;
+    s1.inorder([&](const int& element) { inElements.push_back(element); });
+    std::vector<int> expectedInElements{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (unsigned int i = 0; i < 9; ++i)
+    {
+        EXPECT_EQ(inElements[i], expectedInElements[i]);
+    }
+}
+
+
+TEST(AVLBagTests, postOrderTraversal)
+{
+    AVLBag<int> s1;
+    for (int x = 0; x < 10; ++x)
+    {
+        s1.add(x);
+    }
+    std::vector<int> postElements;
+    s1.postorder([&](const int& element) { postElements.push_back(element); });
+    std::vector<int> expectedPostElements{0, 2, 1, 4, 6, 5, 9, 8, 7, 3};
+
+    for (unsigned int i = 0; i < 9; ++i)
+    {
+        EXPECT_EQ(postElements[i], expectedPostElements[i]);
+    }
+}
+
+
+TEST(AVLBagTests, speedTest)
+{
+    AVLBag<int> s1{};
+    for (int x = 0; x < 100; ++x)
+    {
+        s1.add(x);
+    }
+}
+
